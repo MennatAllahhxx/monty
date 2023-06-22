9fb0ccdac293f705cf67223ce7534c671db0e1f1
Menna Hassan <h.menna85@gmail.com>
2023-06-22 16:27:46 -0700

    task 2

---
+++ task2.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * ppop - fun to pop
+ * @stack: stack
+ * @len: line
+ */
+void ppop(stack_t **stack, unsigned int len)
+{
+	stack_t *st;
+
+	st = *stack;
+	if (!(*stack))
+	{
+		printf("L%u: can't pop an empty stack\n", len);
+		free(stack);
+		exit(EXIT_FAILURE);
+	}
+	*stack = (*stack)->next;
+	free(st);
+}
